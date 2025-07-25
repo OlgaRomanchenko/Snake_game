9e0b9fc6da08bd2ef41e2dc7bb1d6f1ef4eaf709
OlgaRomanchenko <olgaromanch3@gmail.com>
2025-07-25 21:25:44 +0300

    Adding and updating HighScore using data.txt file

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ FONT = ("Courier", 23, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.penup()
         self.goto(0, 270)
@@ -16,11 +18,16 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
 
     def update_scoreboard(self):
-        self.write(f" Score {self.score}", align= ALIGNMENT, font= FONT)
+        self.write(f" Score {self.score},Hight Score {self.high_score}", align= ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(" Game Over ", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode= "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
